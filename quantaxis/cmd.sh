72374b366b0daaaf2796d0903ebe310dd2f832b6
barretthugh <barrettyip@gmail.com>
2018-06-02 01:31:08 +0800

    update

--- quantaxis/cmd.sh
+++ quantaxis/cmd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 forever start /QUANTAXIS/QUANTAXIS_Webkit/backend/bin/www
-forever start /QUANTAXIS/QUANTAXIS_Webkit/web/build/dev-server.js
+cd /QUANTAXIS/QUANTAXIS_Webkit/web/
+npm run dev
